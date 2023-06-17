eec02d114fb498a3a53203c62e388fef4671b1fa
ayush7088 <ayushjoshi282@gmail.com>
2023-06-17 13:02:55 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+
+
+const App = () =>{
+    return (
+     <><h1>FUCK THIS SHIT</h1></> 
+    )  
+  }
+
+  export default App;
+  
